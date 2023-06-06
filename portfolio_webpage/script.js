458e9598ceab434bea10605ed45937a050806eb3
yzdgit <alyazidpro@gmail.com>
2023-06-06 16:03:25 +0300

    p0

--- portfolio_webpage/script.js
+++ portfolio_webpage/script.js
@@ -4,17 +4,14 @@ function addRecommendation() {
   // If the user has left a recommendation, display a pop-up
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
-    //Call showPopup here
+    showPopup(true);
 
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
     document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
+    recommendation.value = "New recommendation added";
   }
 }
 
