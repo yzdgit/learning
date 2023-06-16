7cf496f36d0a35326b7cec29123ee0986e5066e4
yzdgit <alyazidpro@gmail.com>
2023-06-16 20:58:25 +0300

    removed app with APIs

--- listings/README.md
+++
@@ -1,4 +0,0 @@
-# Listings App
-- Build a listings app using flutter
-- Have Rust Actix Web as a webserver for authentication
-- Have MongoDB as a cloud hosted database 
--- listings/lib/dbHelper/constant.dart
+++
@@ -1,7 +0,0 @@
-// user = "alyazid";
-// password = "testTrial";
-
-const MONGO_CONN_URL =
-    "mongodb+srv://alyazid:testTrial@cluster0.i3kt3iu.mongodb.net/?retryWrites=true&w=majority"; // Connect
-const USER_COLLECTION = "Cluster0"; // Cluster
-
