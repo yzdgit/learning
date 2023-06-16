35a1bb30c4aa03de1dfe4ac1bca670ceb80cefc0
yzdgit <alyazidpro@gmail.com>
2023-06-16 20:46:18 +0300

    layout

---
+++ listings/lib/dbHelper/constant.dart
@@ -0,0 +1,7 @@
+// user = "alyazid";
+// password = "testTrial";
+
+const MONGO_CONN_URL =
+    "mongodb+srv://alyazid:testTrial@cluster0.i3kt3iu.mongodb.net/?retryWrites=true&w=majority"; // Connect
+const USER_COLLECTION = "Cluster0"; // Cluster
+
